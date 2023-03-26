5524efed2c320cba5785e3c12be1acce845a6415
tommyguo <tommyguo77@gmail.com>
2023-03-25 21:07:15 -0400

    Moved joint system into Arm Subsystem.

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -13,34 +13,63 @@ import edu.wpi.first.wpilibj.Joystick;
 public class ArmSubsystem {
     Joystick gamePad = new Joystick(appendix.driveControllerID);
 
+    // Motors & Data for main arm.
     CANSparkMax masterArmMotor = new CANSparkMax(appendix.motorArm1, MotorType.kBrushless);
     CANSparkMax secondaryArmMotor = new CANSparkMax(appendix.motorArm2, MotorType.kBrushless);
 
-    double targetPosition = 0;
-    RelativeEncoder motorEncoder1 = masterArmMotor.getEncoder();
+    double armTargetPosition = 0;
+    RelativeEncoder armMotorEncoder = masterArmMotor.getEncoder();
+
+    // Motors & Data for arm joint.
+    CANSparkMax armJointMotor = new CANSparkMax(appendix.motorArm1, MotorType.kBrushless);
+
+    double armJointTargetPosition = 0;
+    RelativeEncoder armJointMotorEncoder = masterArmMotor.getEncoder();
 
     public ArmSubsystem() {
+        armJointMotor.setIdleMode(IdleMode.kBrake);
         masterArmMotor.setIdleMode(IdleMode.kBrake);
         secondaryArmMotor.setIdleMode(IdleMode.kBrake);
-
         secondaryArmMotor.follow(masterArmMotor, true);
-        motorEncoder1.setPosition(-1);
+
+        armJointMotorEncoder.setPosition(-1);
+        armMotorEncoder.setPosition(-1);
     }
 
     public void teleopPeriodic() {
+        ArmPeriodic();
+        JointPeriodic();
+    }
+
+    public void ArmPeriodic() {
         if (gamePad.getRawButton(appendix.buttonY)) {
-            targetPosition = appendix.level3;
+            armTargetPosition = appendix.level3;
         } else if (gamePad.getRawButton(appendix.buttonB)) {
-            targetPosition = appendix.level2;
+            armTargetPosition = appendix.level2;
         } else if (gamePad.getRawButton(appendix.buttonA)) {
-            targetPosition = appendix.level1;
+            armTargetPosition = appendix.level1;
         }
 
         if (Math.abs(gamePad.getRawAxis(appendix.axisRightY)) > 0.05) {
             masterArmMotor.set(gamePad.getRawAxis(appendix.axisRightY) * -0.85);
-            targetPosition = motorEncoder1.getPosition();
+            armTargetPosition = armMotorEncoder.getPosition();
         } else {
-            common.moveToPosition(targetPosition, motorEncoder1.getPosition(), masterArmMotor);
+            common.moveToPosition(armTargetPosition, armMotorEncoder.getPosition(), masterArmMotor);
         }
+    }
+
+    public void JointPeriodic() {
+        if (Math.abs(getArmTrigger()) > 0.05) {
+            armJointMotor.set(getArmTrigger() * 0.35);
+            armJointTargetPosition = armJointMotorEncoder.getPosition();
+        } else {
+            common.moveToPosition(armJointTargetPosition, armJointMotorEncoder.getPosition(), armJointMotor);
+        }
+    }
+
+    private double getArmTrigger() {
+        double rightValue = gamePad.getRawAxis(appendix.triggerRight);
+        double leftValue =  gamePad.getRawAxis(appendix.triggerLeft);
+        return (rightValue > leftValue ? rightValue * -1 : leftValue);
     }
 }
--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,40 +1,16 @@
 package frc.robot.subsystems;
 
 import frc.robot.helpers.appendix;
-import frc.robot.helpers.common;
-
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.RelativeEncoder;
-import com.revrobotics.CANSparkMax.IdleMode;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.Joystick;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 public class IntakeSubsystem {
     Joystick gamePad = new Joystick(appendix.driveControllerID);
 
-    CANSparkMax intakeMotor = new CANSparkMax(7, MotorType.kBrushless);
-
-    double targetPosition = 0;
-    RelativeEncoder intakeEncoder = intakeMotor.getEncoder();
-
-    public IntakeSubsystem() {
-        intakeMotor.setIdleMode(IdleMode.kBrake);
-        intakeEncoder.setPosition(-1);
-    }
-
-    private double getArmTrigger() {
-        double rightValue = gamePad.getRawAxis(appendix.triggerRight);
-        double leftValue =  gamePad.getRawAxis(appendix.triggerLeft);
-        return (rightValue > leftValue ? rightValue * -1 : leftValue);
-    }
+    WPI_VictorSPX intakeMotor = new WPI_VictorSPX(appendix.motorLeft1);
 
     public void teleopPeriodic() {
-        if (Math.abs(getArmTrigger()) > 0.05) {
-            intakeMotor.set(getArmTrigger() * 0.35);
-            targetPosition = intakeEncoder.getPosition();
-        } else {
-            common.moveToPosition(targetPosition, intakeEncoder.getPosition(), intakeMotor);
-        }
+        intakeMotor.set(gamePad.getRawButton(appendix.buttonRight) ? 1 : -1);
     }
 }
